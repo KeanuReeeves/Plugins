268a475708ea6cf7d627b1f6b3274dd6e66d1199
Katona Kristóf <csakimark2000@outlook.com>
2022-01-15 12:39:39 +0100

    kezdeti validáció, email ellenőrzés

--- Plugins/CreatePreValidation.cs
+++ Plugins/CreatePreValidation.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
 
 namespace Plugins
 {
-    class CreatePreValidation
+    class CreatePreValidation : IPlugin
     {
-//teszt2
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            string logicalName = "account";
+            string email = "email";
+            string correctFormat = "correctFormat";
+            bool b;
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+                b = entity.Attributes[email].ToString().Contains(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                if (entity.LogicalName == logicalName)
+                {
+                    if (entity.Attributes.Contains(email) && b)
+                    {
+                        entity.Attributes.Add(correctFormat, true);
+                    }
+                    else
+                    {
+                        entity.Attributes.Add(correctFormat, false);
+                    }
+                }
+            }
+        }
     }
 }
